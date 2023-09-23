793e79a06b0ac56d77648e6aa9c1baf5b48fe439
yasingunay <av.yasingunay@gmail.com>
2023-09-23 10:32:39 +0200

    send mail added

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector("#compose-form").onsubmit = () => {send_email();
+    // Stop form from submitting
+    return false;
+  };
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +34,27 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+
+
+function send_email() {
+    let recipients = document.querySelector("#compose-recipients").value;
+    let subject = document.querySelector("#compose-subject").value;
+    let body = document.querySelector("#compose-body").value;
+
+    fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+        })
+     })
+    .then(response => response.json())
+    .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('sent')
+    });
 }
